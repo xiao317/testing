3624a07176b2c4943e9f5526abcb2cadfeba691e
Xiao Liu <xiaoliu@xiaos-mbp.myfiosgateway.com>
2020-10-15 15:21:15 -0400

    hello

---
+++ datatypes.py
@@ -0,0 +1,15 @@
+# integer data type
+age = 150_000
+print(age)
+
+# float data type
+howmany_lit_of_milk = 3.5
+print(howmany_lit_of_milk)
+
+# string data type
+name = "Xiao"
+print(name, type(name))
+
+# boolean data type
+is_rain = True
+print(is_rain, type(is_rain))
